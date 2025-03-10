ee28d06eafef56a5f47dec01f2b0296041454952
trantrungnghiadev1998 <trantrungnghiadev1998.com>
2025-03-10 21:56:41 +0700

    style: Improve form layout and common elements

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const passport = require("passport");
 const session = require("express-session");
 require("./config/passportConfig");
 
+// Allow to access static files in "public" folder
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
